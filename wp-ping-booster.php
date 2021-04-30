f9c7eabbcc69e22b0dc380e38d7598b1f45abc42
Eugen <pustojack@list.ru>
2021-04-30 12:33:41 +0300

    Create wp-ping-booster.php

---
+++ wp-ping-booster.php
@@ -0,0 +1,11 @@
+/*
+ * Plugin name: WP Ping Booster
+ * Description: When you publish new content or update old content on your WordPress website, the plugin sends a crawl request to Bing using the Bing Indexing API.
+ * Version: 2.2.2
+ * Author: seojacky 
+ * Author URI: https://t.me/big_jacky
+ * Plugin URI: https://wordpress.org/plugins/true-lazy-analytics/
+ * GitHub Plugin URI: https://github.com/seojacky/true-lazy-analytics
+ * Text Domain: true-lazy-analytics
+ * Domain Path: /languages
+*/
